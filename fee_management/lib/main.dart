c1baaae5f6fb2c560ff74872c4ea4333c82f68c3
Muhammed <ojomuhammad@gmail.com>
2025-04-16 14:11:55 +0100

    Added api structure

--- fee_management/lib/main.dart
+++ fee_management/lib/main.dart
@@ -5,7 +5,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 Future<void> main() async{
-   WidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
